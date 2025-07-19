0f8d06f69fa1fc62f03fe8d0e3f5afd70b7a209d
MarcSab20 <marcfotso20@gmail.com>
2025-07-19 11:25:54 +0100

    gf

--- auth/src/app/page.tsx
+++ auth/src/app/page.tsx
@@ -1,14 +1,3 @@
-import { CallToAction } from '@/src/components/landing-page/CallToAction'
-import { Faqs } from '@/src/components/landing-page/Faqs'
-import { Footer } from '@/src/components/landing-page/Footer'
-import { Header } from '@/src/components/landing-page/Header'
-import { Hero } from '@/src/components/landing-page/Hero'
-// import { Pricing } from '@/src/components/landing-page/Pricing'
-import { PrimaryFeatures } from '@/src/components/landing-page/PrimaryFeatures'
-import { SecondaryFeatures } from '@/src/components/landing-page/SecondaryFeatures'
-import { Testimonials } from '@/src/components/landing-page/Testimonials'
-import  SMPServiceCarousel  from '@/src/components/landing-page/CardCaroussel'
-
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
